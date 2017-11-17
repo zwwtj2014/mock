e532ecfb4b9c6b790fa0ea0f8074fde1b550d8f3
zwwtj2014 <529474551@qq.com>
2017-11-18 00:19:12 +0800

    在第三次迭代时理解整体应用

--- express-es6/lib/express.js
+++ express-es6/lib/express.js
@@ -2,7 +2,7 @@
  * @Author: clam
  * @Date: 2017-11-12 00:20:43
  * @Last Modified by: clam
- * @Last Modified time: 2017-11-18 00:01:51
+ * @Last Modified time: 2017-11-18 00:11:01
  */
 const App = require('./application');
 
@@ -30,5 +30,17 @@ exports = module.exports = createApplication;
     | ... |   ...     |       |  |- handle|
     ----- -----------        -------------
         router
+
+
+    目前(第三次迭代完):
+
+    一个App 对应一个 Router
+
+    ===> Router会根据path创建Layer(主要包含path和Route), 存储在Router的stack属性中,
+    Router中的next函数会根据请求遍历stack匹配相应的处理, 然后通过Route的dispatch方法分发下去
+
+    ===> 目前一个Route会对应一个新的Layer实例(主要包含method和handle), 存储在Route的stack属性中
+    Route中的next函数会根据Router中的匹配到的Route去进行匹配(主要是method), 匹配到之后, 动态调用handle
+
  *
  */
